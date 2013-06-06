d116ef9bbf3f2c2f4de6731e13c653a0426c81c2
yiannis <yiannis@seleucus.net>
2013-06-06 23:39:16 +0000

    Added the first fully working client implementation, need to use mvn clean compile assembly:single to use it

--- src/main/java/net/seleucus/wsp/main/WebSpa.java
+++ src/main/java/net/seleucus/wsp/main/WebSpa.java
@@ -1,6 +1,11 @@
 package net.seleucus.wsp.main;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.Console;
+
+import net.seleucus.wsp.client.WSClient;
 
 public class WebSpa {
 
@@ -22,11 +27,12 @@ public class WebSpa {
 			
 		} // java -jar web-spa.jar -client
 		  else if (args[0].equalsIgnoreCase(ALLOWED_FIRST_PARAM[1])) {
-			
+		
+			  runClient(System.console());
 		} // java -jar web-spa.jar -server
 		  else if (args[0].equalsIgnoreCase(ALLOWED_FIRST_PARAM[2])) {
 			  
-			  runServer();
+			  runServer(System.console());
 			  
 		} // java -jar web-spa.jar -version
 		  else if (args[0].equalsIgnoreCase(ALLOWED_FIRST_PARAM[3])) {
@@ -44,7 +50,7 @@ public class WebSpa {
 	private static void showHelp() {
 
 		System.out.println("");
-		System.out.println("Web-Spa - Single HTTP/S Request Authorisation - version " + WSVersion.getValue() + " (subere@uncon.org)"); 
+		System.out.println("Web-Spa - Single HTTP/S Request Authorisation - version " + WSVersion.getValue() + " (web-spa@seleucus.net)"); 
 		System.out.println("");
 		System.out.println("Usage: java -jar web-spa.jar [-option]");
 		System.out.println("");
@@ -61,13 +67,12 @@ public class WebSpa {
 		
 	}
 	
-	private static void runServer() {
+	private static void runServer(final Console console) {
 
 		System.out.println("");
-		System.out.println("Web-Spa - Single HTTP/S Request Authorisation - version " + WSVersion.getValue() + " (subere@uncon.org)"); 
+		System.out.println("Web-Spa - Single HTTP/S Request Authorisation - version " + WSVersion.getValue() + " (web-spa@seleucus.net)"); 
 		System.out.println("");
 
-		final Console console = System.console();
 		if (console == null) {
 			System.err.println("Could not get console; will exit now...");
 			System.exit(1);
@@ -87,7 +92,51 @@ public class WebSpa {
 
 		} while (true);
 		
-		System.out.println("\nGoodbye!");
+		System.out.println("\nGoodbye!\n");
+		System.exit(0);
+	}
+	
+	private static void runClient(final Console console) {
+
+		System.out.println("");
+		System.out.println("Web-Spa - Single HTTP/S Request Authorisation - version " + WSVersion.getValue() + " (web-spa@seleucus.net)"); 
+		System.out.println("");
+
+		if (console == null) {
+			System.err.println("Could not get console; will exit now...");
+			System.exit(1);
+		}
+
+		String host = console.readLine("Please enter the target host [e.g. http://localhost/]: ");
+		CharSequence password = java.nio.CharBuffer.wrap(console.readPassword("Please enter your pass-phrase for that host: "));
+		int action = -1;
+		do {
+
+			String actionString = console.readLine("Please enter the action you want to execute on the host [1-16]: ");
+			action = Integer.parseInt(actionString);
+			
+		} while ( (action < 1) || (action > 16) );
+		
+		
+		WSClient myClient = new WSClient(host, password, action);
+		String knock = myClient.getKnock();
+		
+		System.out.println("\nYour Web-Spa Knock is:\n\n" + knock + "\n");
+		
+		// Clipboard nonsense 
+		final String choice = console.readLine("Copy the above URL to the clipboard? ");
+				
+		if("yes".equalsIgnoreCase(choice) ||
+			"y".equalsIgnoreCase(choice) ||
+			choice.isEmpty() ) {
+			
+			final StringSelection data = new StringSelection(knock);
+			final Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents(data, data);
+			
+		}
+		
+		System.out.println("\nGoodbye!\n");
 		System.exit(0);
 	}
 
