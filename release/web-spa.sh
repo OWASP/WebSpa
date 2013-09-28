0ecb60b0be6474c4ace796fbe6c2153f6a780b61
yiannis <yiannis@seleucus.net>
2013-09-28 18:24:39 +0000

    Added a release directory with an INSTALL, a README, a LICENSE and a small shell script

---
+++ release/web-spa.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+JAVA=`which java`
+WEB_SPA="web-spa-0.5.jar"
+JAVA_PARAMS="-Xms64m -Xmx256m"
+
+# launch web-spa
+if [ -r ./$WEB_SPA ]; then
+    $JAVA $JAVA_PARAMS -jar ./$WEB_SPA $@
+    exit 0;
+fi
+
+echo ""
+echo "Web-Spa - Single HTTP/S Request Authorisation - version 0.5 (web-spa@seleucus.net)"
+echo ""
+echo "Unable to find $WEB_SPA file."
+echo "$0 needs to be in the same directory as $WEB_SPA"
+echo ""
