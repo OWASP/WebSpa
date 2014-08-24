c31a0ed1e42ee321c9654c66057c37786e94e52e
yns000 <yns000@@seleucus.net>
2014-08-24 18:00:47 +0000

    A first attempt to offer SCrypt for a new web knock format with N = (128 * ( Math.pow(2, sRandom.nextInt(3) ) )), r = 32 + sRandom.nextInt(32), p = 16 + sRandom.nextInt(8)

---
+++ src/main/java/net/seleucus/wsp/crypto/SCryptPassPhraseCrypto.java
@@ -0,0 +1,20 @@
+package net.seleucus.wsp.crypto;
+
+import java.security.SecureRandom;
+
+import com.lambdaworks.crypto.SCryptUtil;
+
+public final class SCryptPassPhraseCrypto extends WebSpaUtils {
+
+	protected static String getHashedPassPhraseWithSalt(final CharSequence passPhrase) {
+		
+		SecureRandom sRandom = new SecureRandom();
+		final int nFactor = (int) (128 * ( Math.pow(2, sRandom.nextInt(3) ) )) ;
+		final int rFactor = 32 + sRandom.nextInt(32);
+		final int pFactor = 16 + sRandom.nextInt(8);
+		
+		return SCryptUtil.scrypt(passPhrase.toString(), nFactor, rFactor, pFactor);
+	    
+	}
+	
+}
---
+++ src/test/java/net/seleucus/wsp/crypto/SCryptPassPhraseCryptoTest.java
@@ -0,0 +1,34 @@
+package net.seleucus.wsp.crypto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.lambdaworks.crypto.SCryptUtil;
+
+public class SCryptPassPhraseCryptoTest {
+
+	// A static password value specified as a char sequence
+	private static final CharSequence PASS_PHRASE = "IfY0u0nlyKn3w!";
+	
+	@Test
+	public final void shouldImplementThisAfterWeDecideOnTheWorkFactors() {
+		
+		final String hashedPassPhraseWithSalt = SCryptPassPhraseCrypto.getHashedPassPhraseWithSalt(PASS_PHRASE);
+		assertTrue(SCryptUtil.check(PASS_PHRASE.toString(), hashedPassPhraseWithSalt));
+		
+	}
+	/*
+	@Test
+	public final void shouldTestRandomValueGeneration() {
+		
+		SecureRandom sRandom = new SecureRandom();
+		final int nFactor = (int) (128 * ( Math.pow(2, sRandom.nextInt(3) ) )) ;
+		final int rFactor = 32 + sRandom.nextInt(32);
+		final int pFactor = 16 + sRandom.nextInt(8);
+		
+		fail("Factors:\nN = " + nFactor + "\nr = " + rFactor + "\np = " + pFactor);
+	}
+	*/
+
+}
