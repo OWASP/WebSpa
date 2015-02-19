6155922e7d459894a5b4d5aa9583d41923c1a9c8
imberda <danimber@googlemail.com>
2015-02-19 23:39:16 +0000

    Refactor of array equals method to make re-use of standard Java Arrays.equals method.

--- src/main/java/net/seleucus/wsp/crypto/WebSpaUtils.java
+++ src/main/java/net/seleucus/wsp/crypto/WebSpaUtils.java
@@ -2,67 +2,34 @@ package net.seleucus.wsp.crypto;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.Arrays;
+
 public class WebSpaUtils {
 
 	private static final int ITERATIONS = 1024;
 	
 	protected static byte[] digest(byte[] value) {
-		
 		for (int i = 0; i < ITERATIONS; i++) {
 			value = DigestUtils.sha512(value);
         }
         
 		return value;
-		
     }
 	
 	protected static byte[] xor(final byte[] inputByteArray, final byte timeByte) {
-
-		byte[] outputByteArray = new byte[inputByteArray.length];
+		final byte[] outputByteArray = new byte[inputByteArray.length];
 
 		for (int intCount = 0; intCount < inputByteArray.length; intCount++) {
 			outputByteArray[intCount] = (byte) (inputByteArray[intCount] ^ timeByte);
 		}
 		
 		return outputByteArray;
-		
 	}
 	
 	protected static boolean equals(byte[] first, byte[] second) {
-		
-		boolean arraysAreEqual = false;
-				
-		boolean foundOneValueThatDoesNotMatch = false;
-		
-		if(first == null || second == null) {
-			
-			arraysAreEqual = false;
-			
-		} else {
-
-			int maximum = (first.length < second.length) ? first.length : second.length;
-
-			for(int count = 0; count < maximum; count++) {
-				
-				if(first[count] != second[count]) {
-					
-					foundOneValueThatDoesNotMatch = true;
-					
-				}
-			}
-
-			arraysAreEqual = ! (foundOneValueThatDoesNotMatch);
-
-			if(first.length != second.length) {
-				
-				arraysAreEqual = false;
-				
-			}
-		}
-		
-		
-		return arraysAreEqual;
-		
+        if(first == null && second == null){
+            return false;
+        }
+		return Arrays.equals(first, second);
 	}
-
 }
