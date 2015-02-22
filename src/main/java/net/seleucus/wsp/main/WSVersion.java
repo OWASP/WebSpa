f52aa8252379f71729d0f0f2911b054aef353302
imberda <danimber@googlemail.com>
2015-02-22 10:23:10 +0000

    Making version a dynamic property driven from the project's maven version, rather than hardcoded into java class file.

--- src/main/java/net/seleucus/wsp/main/WSVersion.java
+++ src/main/java/net/seleucus/wsp/main/WSVersion.java
@@ -1,29 +1,56 @@
 package net.seleucus.wsp.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WSVersion extends WSGestalt {
 
-	private static final byte[] VERSION = {0,8};
+    private static final Logger logger = LoggerFactory.getLogger(WSVersion.class);
 
-	public WSVersion(WebSpa myWebSpa) {
-		super(myWebSpa);
-	}
-	
-	public static int getMajor() {
-		return VERSION[0];
-	}
-	
+    public static final String VERSION_PROPERTIES_FILE = "/config/version.properties";
+    public static final String MAJOR_VERSION_KEY = "major.version";
+    public static final String MINOR_VERSION_KEY = "minor.version";
+
+
+    private static int majorVersion;
+    private static int minorVersion;
+
+    static {
+        try (final InputStream inputStream = WSVersion.class.getResourceAsStream(VERSION_PROPERTIES_FILE)) {
+            final Properties properties = new Properties();
+            properties.load(inputStream);
+            majorVersion = Integer.valueOf(properties.getProperty(MAJOR_VERSION_KEY, "0"));
+            minorVersion = Integer.valueOf(properties.getProperty(MINOR_VERSION_KEY, "0"));
+
+        } catch (IOException ioe){
+            majorVersion = 0;
+            minorVersion = 0;
+        }
+    }
+
+    public WSVersion(WebSpa myWebSpa) {
+        super(myWebSpa);
+    }
+
+    public static int getMajor() {
+        return majorVersion;
+    }
 	public static int getMinor() {
-		return VERSION[1];
+		return minorVersion;
 	}
-	
+
 	public static String getValue() {
-		return VERSION[0] + "." + VERSION[1];
+		return majorVersion + "." + minorVersion;
 	}
-	
+
 	public static boolean isCurrentVersion(final String versionString) {
 		return WSVersion.getValue().equalsIgnoreCase(versionString);
 	}
-	
+
 	@Override
 	public void exitConsole() {
 		// Nothing to add here...
@@ -33,5 +60,4 @@ public class WSVersion extends WSGestalt {
 	public void runConsole() {
 		myConsole.println(WSVersion.getValue());
 	}
-	
 }
--- src/test/java/net/seleucus/wsp/main/WSVersionTest.java
+++ src/test/java/net/seleucus/wsp/main/WSVersionTest.java
@@ -1,30 +1,27 @@
 package net.seleucus.wsp.main;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import net.seleucus.wsp.console.WSConsole;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.sql.SQLException;
 
-import net.seleucus.wsp.console.WSConsole;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class WSVersionTest {
 
 	private static int VERSION_MIN = 0;
 	private static int VERSION_MAX = 10;
-	
+
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
 	@Before
 	public void setUpStreams() {
-	
+
 		System.setOut(new PrintStream(outContent));
 	    System.setErr(new PrintStream(errContent));
 
@@ -35,55 +32,55 @@ public class WSVersionTest {
 	    System.setOut(null);
 	    System.setErr(null);
 	}
-	
+
 	@Test
 	public final void testGetMajor() {
 		int majorVersion = WSVersion.getMajor();
-		
+
 		assertTrue(
 			"Major version is between [0,10): " + majorVersion,
-			( (majorVersion >= VERSION_MIN) && (majorVersion < VERSION_MAX) )	
+			( (majorVersion >= VERSION_MIN) && (majorVersion < VERSION_MAX) )
 		);
 	}
 
 	@Test
 	public final void testGetMinor() {
 		int minorVersion = WSVersion.getMinor();
-		
+
 		assertTrue(
 			"Major version is between [0,10): " + minorVersion,
-			( (minorVersion >= VERSION_MIN) && (minorVersion < VERSION_MAX) )	
-		);	
+			( (minorVersion >= VERSION_MIN) && (minorVersion < VERSION_MAX) )
+		);
 	}
 
 	@Test
 	public final void testGetValue() {
 		assertEquals(WSVersion.getValue(), WSVersion.getMajor() + "." + WSVersion.getMinor());
 	}
-	
+
 	@Test
 	public final void shouldBe3Characters() {
 		String version = WSVersion.getValue();
 		assertEquals(3, version.length());
 	}
-	
+
 	@Test
 	public final void testIsValidTrue() {
 		assertTrue(WSVersion.isCurrentVersion("0.8"));
 	}
-	
+
 	@Test
 	public final void testIsValidFalse() {
 		assertFalse(WSVersion.isCurrentVersion("0.4"));
 	}
-	
+
 	@Test
 	public void testRunConsole() throws SQLException {
-		
+
 		WSVersion myVersion = new WSVersion(new WebSpa(WSConsole.getWsConsole()));
 		myVersion.runConsole();
 		assertEquals(WSVersion.getValue(), outContent.toString().trim());
-		
+
 	}
-	
+
 }
