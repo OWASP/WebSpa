c0b3c07ee7dbc8702bfbd2b105170b0c0d6a4dd2
tukan68 <github.com@reg.rouiller.org>
2015-05-17 15:21:22 +0200

    Known hosts file implementation

--- src/main/java/net/seleucus/wsp/client/WSClient.java
+++ src/main/java/net/seleucus/wsp/client/WSClient.java
@@ -1,8 +1,17 @@
 package net.seleucus.wsp.client;
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.security.cert.CertificateEncodingException;
+
+import javax.net.ssl.SSLPeerUnverifiedException;
+
 import net.seleucus.wsp.main.WSGestalt;
 import net.seleucus.wsp.main.WSVersion;
 import net.seleucus.wsp.main.WebSpa;
+import net.seleucus.wsp.util.WSKnownHosts;
 import net.seleucus.wsp.util.WSUtil;
 
 import org.slf4j.Logger;
@@ -10,94 +19,118 @@ import org.slf4j.LoggerFactory;
 
 public class WSClient extends WSGestalt {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(WSClient.class);    
+	private final static Logger LOGGER = LoggerFactory.getLogger(WSClient.class);
 
 	public WSClient(final WebSpa myWebSpa) {
-		super(myWebSpa);		
+		super(myWebSpa);
 	}
-	
+
+	@Override
+	public void exitConsole() {
+		LOGGER.info("Goodbye!");
+	}
+
 	@Override
 	public void runConsole() {
-		
+
 		LOGGER.info("");
 		LOGGER.info("WebSpa - Single HTTP/S Request Authorisation");
-		LOGGER.info("version " + WSVersion.getValue() + " (webspa@seleucus.net)"); 		
+		LOGGER.info("version " + WSVersion.getValue() + " (webspa@seleucus.net)");
 		LOGGER.info("");
 
 		String host = readLineRequired("Host [e.g. https://localhost/]");
 		CharSequence password = readPasswordRequired("Your pass-phrase for that host");
 		int action = readLineRequiredInt("The action number", 0, 9);
-		
+
 		WSRequestBuilder myClient = new WSRequestBuilder(host, password, action);
 		String knock = myClient.getKnock();
-		
+
 		LOGGER.info("Your WebSpa Knock is: {}", knock);
-		
-		// URL nonsense 
+
+		// URL nonsense
 		final String sendChoice = readLineOptional("Send the above URL [Y/n]");
-		
-		if ( WSUtil.isAnswerPositive(sendChoice) || sendChoice.isEmpty() ) {
-			
+
+		if (WSUtil.isAnswerPositive(sendChoice) || sendChoice.isEmpty()) {
+
 			WSConnection myConnection = new WSConnection(knock);
-			
+
 			LOGGER.info(myConnection.getActionToBeTaken());
-		
+
 			myConnection.sendRequest();
 
 			// is the connection HTTPS
-			if( myConnection.isHttps() ) {
-				// TODO add known hosts check and handling here
-				// get fingerprint and algorithm from certificate
-				// myConnection.getCertificateAlgorithm()
-				// myConnection.getCertificateFingerprint()
-
-				// get fingerprint from known hosts file
-				// WSKnownHosts.getFingerprint(host-ip, algorithm)
-
-				// if a fingerprint is found compare fingerprints, if not equal warn and exit
-				// else ask to store new fingerprint to known hosts
-				// WSKnownHosts.store...(host-ip, algorithm, fingerprint);
-
+			if (myConnection.isHttps()) {
 				try {
-					
+					WSKnownHosts knownHosts = new WSKnownHosts();
+
 					LOGGER.info(myConnection.getCertSHA1Hash());
-					
+
+					String ipAddr = getIpAddr(host);
+					String certAlgorithm = myConnection.getCertificateAlgorithm();
+					String certFingerprint = myConnection.getCertificateFingerprint();
+
+					Boolean fingerprintMatches = knownHosts.check(ipAddr, certAlgorithm, certFingerprint);
+
+					if (fingerprintMatches == null) {
+						final String storeNewFingerprint = readLineOptional("New fingerprint, add it to the WebSPA known hosts file [Y/n]");
+
+						if (WSUtil.isAnswerPositive(storeNewFingerprint)) {
+							knownHosts.addKnownHost(ipAddr, certAlgorithm, certFingerprint);
+						}
+					} else if (fingerprintMatches.booleanValue()) {
+						LOGGER.info("fingerprint matches");
+					} else {
+						myConsole.println("WARNING: The certificate fingerprint does NOT match with the known hosts file");
+					}
+
 				} catch (NullPointerException npEx) {
-					
 					LOGGER.info("Couldn't get the SHA1 hash of the server certificate - probably a self signed certificate.");
-					
+
 					if (!WSUtil.hasMinJreRequirements(1, 7)) {
 						LOGGER.error("Be sure to run WebSpa with a JRE 1.7 or greater.");
 					} else {
 						LOGGER.error("An exception was raised when reading the server certificate.");
 						npEx.printStackTrace();
 					}
+				} catch (SSLPeerUnverifiedException e) {
+					e.printStackTrace();
+				} catch (CertificateEncodingException e) {
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
 				}
 
 				final String trustChoice = readLineOptional("Continue connecting [Y/n]");
-				
-				if( WSUtil.isAnswerPositive(trustChoice) || sendChoice.isEmpty() ) {
-					
+
+				if (WSUtil.isAnswerPositive(trustChoice) || sendChoice.isEmpty()) {
+
 					myConnection.sendRequest();
 					LOGGER.info(myConnection.responseMessage());
 					LOGGER.info("HTTPS Response Code: {}", myConnection.responseCode());
-					
+
 				} else {
-					
+
 					LOGGER.info("Nothing was sent.");
-					
+
 				}
-				
+
 			} else {
-				
+
 				myConnection.sendRequest();
 				LOGGER.info(myConnection.responseMessage());
 				LOGGER.info("HTTP Response Code: {}", myConnection.responseCode());
-	
+
 			}
-						
+
 		}
 
+	}
+
+	private String getIpAddr(String host) throws MalformedURLException, UnknownHostException {
+		URL url = new URL(host);
+		return InetAddress.getByName(url.getHost()).getHostAddress();
 	}
 
 }
--- src/main/java/net/seleucus/wsp/util/WSKnownHosts.java
+++ src/main/java/net/seleucus/wsp/util/WSKnownHosts.java
@@ -2,25 +2,75 @@ package net.seleucus.wsp.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
 
+/**
+ * This class manages the known hosts file in the .webspa folder in the users
+ * home folder.<br/>
+ * Each line in the file is an entry for a given IP address and the information
+ * is separated by a space. The information in the file is
+ * <ul>
+ * <li>IP address</li>
+ * <li>Algorithm</li>
+ * <li>Certificate finger print</li>
+ * </ul>
+ */
 public class WSKnownHosts {
 
+	private String userWebSpaKnownHostsFileName;
 	private File userWebSpaKnownHostsFile;
 
 	public WSKnownHosts() {
-		String userWebSpaKnownHostsFileName = System.getProperty("user.home")
-				+ File.separator + ".webspa" + File.separator + "known_hosts";
-
+		userWebSpaKnownHostsFileName = System.getProperty("user.home") + File.separator + ".webspa" + File.separator + "known_hosts";
 		userWebSpaKnownHostsFile = new File(userWebSpaKnownHostsFileName);
 	}
 
-	public boolean checkKnownHost(final URL url, final String fingerprint) {
+	/**
+	 * Adds an entry to the users known hosts file.
+	 * 
+	 * @throws RuntimeException
+	 *             if the known hosts file cannot be created or entries added.
+	 */
+	public void addKnownHost(final String ipAddr, final String certAlgorithm, final String certFingerprint) {
+		if (!userWebSpaKnownHostsFile.exists()) {
+			try {
+				userWebSpaKnownHostsFile.getParentFile().mkdirs();
+				userWebSpaKnownHostsFile.createNewFile();
+			} catch (IOException ioEx) {
+				throw new RuntimeException("Couldn't create the WebSPA known hosts file: " + userWebSpaKnownHostsFileName);
+			}
+		}
 
-		boolean knownHost = false;
+		StringBuilder newEntry = new StringBuilder();
+		newEntry.append(ipAddr);
+		newEntry.append(" ");
+		newEntry.append(certAlgorithm);
+		newEntry.append(" ");
+		newEntry.append(certFingerprint);
+		newEntry.append(System.lineSeparator());
+
+		try {
+			FileWriter fw = new FileWriter(userWebSpaKnownHostsFile, true);
+			fw.append(newEntry);
+			fw.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Problems adding a new entry to the WebSPA known hosts file: " + userWebSpaKnownHostsFileName);
+		}
+	}
+
+	/**
+	 * Checks if this entry already exists in the users known hosts file.<br/>
+	 * The possible return values are:
+	 * <ul>
+	 * <li>NULL: no information for the given IP address exists
+	 * <li>TRUE: information found and finger print matches
+	 * <li>FALSE: information found but finger print doesn't match
+	 * </ul>
+	 */
+	public Boolean check(final String ipAddr, final String certAlgorithm, final String certFingerprint) {
+		Boolean fingerprintFits = null;
 
 		if (userWebSpaKnownHostsFile.exists()) {
 			FileReader fr;
@@ -31,30 +81,27 @@ public class WSKnownHosts {
 
 				String line;
 				while ((line = br.readLine()) != null) {
-					String urlHash[] = line.split("=");
+					String urlAlgoHash[] = line.split(" ");
 
-					if (url.getHost() == urlHash[0]
-							&& fingerprint == urlHash[1]) {
-						knownHost = true;
-						// TODO what if url ok, but fingerprint not? -> return a 'state'
+					if (ipAddr.equals(urlAlgoHash[0]) && certAlgorithm.equals(urlAlgoHash[1])) {
+						if (certFingerprint.equals(urlAlgoHash[2])) {
+							fingerprintFits = Boolean.TRUE;
+						} else {
+							fingerprintFits = Boolean.FALSE;
+						}
+
 						break;
 					}
 				}
 
 				br.close();
 				fr.close();
-			} catch (FileNotFoundException fnfEx) {
-
 			} catch (IOException ioEx) {
-
+				throw new RuntimeException("Problems reading the WebSPA known hosts file: " + userWebSpaKnownHostsFileName);
 			}
 		}
 
-		return knownHost;
-	}
-
-	public void addKnownHost(final URL url, final String fingerprint) {
-
+		return fingerprintFits;
 	}
 
 }
